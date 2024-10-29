b7d71fc1649547489b4cc6dba64c6f731546688c
Miguel Mayans <miguelmayansv@gmail.com>
2024-10-29 20:31:19 +0100

    added: CV to constants

--- src/constants.js
+++ src/constants.js
@@ -17,8 +17,8 @@ export const dialogueData = {
 
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
 
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-    Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  resume: `Este es mi escritorio y aquí podeis ver mi CV <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">CV de Miky</a>
+    Puedes contactarme por<a href="mailto:miguelmayansv@gmail.com" >mail</a>  `,
 
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
